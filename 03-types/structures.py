fbaf38e5ae8860b5bea4b824e8a55d264d817af4
Dvojak <118245733+Dvojak@users.noreply.github.com>
2024-11-11 21:19:26 +0100

    Add files via upload

--- 03-types/structures.py
+++ 03-types/structures.py
@@ -1,9 +1,9 @@
-message = input("Enter a message: ").strip()
+message = input("Zadej zprávu: ").strip()
 seperate = sorted([(letter, message.count(letter)) for letter in set(tuple(message))], key=lambda item: item[1], reverse=True)
 print(f"Věta: {message}")
 print("Četnost výskytu písmen:")
 print("-----------------------")
-print("[", end="")
-for item in seperate[:-1]:
-    print(f"{item},", end="\n")
-print(f"{seperate[-1]}]")
+print("[")
+for item in seperate:
+    print(f"{item},")
+print("]")
